38ae1ff5a86d3a4ecef5e7dae72d8cad2aa26eec
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-31 13:26:27 +0300

    Edit Nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import BooksContainer from './components/BooksContainer';
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <BooksContainer />
+    <div className='container-fluid'>
+      <Nav />
+      <div className="container mt-2">
+        <BooksContainer />
+      </div>
     </div>
   );
 }
--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,17 +1,20 @@
 import React from 'react'
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+// import InputGroup from 'react-bootstrap/InputGroup';
 
 const Nav = () => {
-  return (
-      <div>
-          <nav className="navbar navbar-light bg-light justify-content-between">
-              <a className="navbar-brand">Navbar</a>
-              <form className="form-inline">
-                  <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
-          </nav>
-    </div>
-  )
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-light justify-content-between">
+                <a className="navbar-brand">Edward's BookStore</a>
+                <form className="d-flex flex-row form-inline">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </nav>
+        </div>
+    )
 }
 
 export default Nav
