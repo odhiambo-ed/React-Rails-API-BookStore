82044ad66998a4146d3a994a81968cd81b1924f8
white3d <odhiambo.edward111@gmail.com>
2022-11-30 14:18:39 +0300

    Edit NavBar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -4,7 +4,7 @@ const Nav = () => {
     return (
         <div className="nav-color p-2">
             <nav className="navbar navbar-light justify-content-between">
-                <a className="navbar-brand">Edward's BookStore></a>
+                <a className="navbar-brand">Edward's BookStore</a>
                 <form className="d-flex flex-row form-inline">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-success my-2 my-sm-0 m-2" type="submit">Search</button>
