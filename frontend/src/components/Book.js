f6512c2d5e1c44f0b115326c91e1af3ea0ffeb55
white3d <odhiambo.edward111@gmail.com>
2022-11-30 12:53:32 +0300

    Edit DB YML

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -14,16 +14,6 @@ const Book = ({ book, onRemoveBook = (f) => f, editingBook = (f) => f }) => (
                     <button className="btn btn-success" onClick={() => editingBook(book.id)}>Edit</button>
                 </div>
             </div>
-
-        {/* <p>
-            <b>{book.title}</b> by {book.author}
-            <br />
-            Genre: {book.genre}
-        </p>
-        <div className="buttons">
-            <button onClick={() => onRemoveBook(book.id)}>Delete</button>
-            <button onClick={() => editingBook(book.id)}>Edit</button>
-        </div> */}
     </div>
 );
 
